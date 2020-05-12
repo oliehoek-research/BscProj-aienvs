874357ef5221a3731db1fc3bccfd0c35601f87a6
Mpcvanderwerf <m.p.c.vanderwerf@student.tudelft.nl>
2020-05-12 17:54:05 +0200

    set sensible default option

--- aienvs/Sumo/SumoGymAdapter.py
+++ aienvs/Sumo/SumoGymAdapter.py
@@ -38,7 +38,7 @@ class SumoGymAdapter(Env):
                 'route_generation_method': 'undefined', # One of ['legacy', 'randomTrips.py', 'activitygen']
 
                 # Options for 'route_generation_method' 'randomTrips.py'
-                'trips_generate_options': [], # sumo/tools/randomTrips.py additional options. -n, -o already handled!
+                'trips_generate_options': ["--validate"], # sumo/tools/randomTrips.py additional options. -n, -o already handled!
 
                 # Custom route and trip generation if route_generation_method is set to 'legacy'
                 'car_pr': 0.5,  # for automatic route/config generation probability that a car appears
